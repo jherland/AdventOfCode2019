982a9f277b5ec5192a8244586a9531789133e048
Johan Herland <johan@herland.net>
2019-12-03 11:39:43 +0100

    Day 3 in Python

---
+++ 03.py
@@ -0,0 +1,36 @@
+def parse_move(s):
+    d, l = s[0], int(s[1:])
+    assert d in {'U', 'R', 'D', 'L'}
+    assert l >= 0
+    return d, l
+
+
+def parse_wires(f):
+    yield from (map(parse_move, line.rstrip().split(',')) for line in f)
+
+
+def step(pos, dir):
+    dx, dy = {'U': (0, +1), 'R': (+1, 0), 'D': (0, -1), 'L': (-1, 0)}[dir]
+    return pos[0] + dx, pos[1] + dy
+
+
+def trace_moves(moves, pos=(0, 0)):
+    for d, l in moves:
+        for _ in range(l):
+            pos = step(pos, d)
+            yield pos
+
+
+def mhdist(p1, p2=(0, 0)):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+with open('03.input') as f:
+    w1, w2 = [list(trace_moves(moves)) for moves in parse_wires(f)]
+crossings = set(w1) & set(w2)
+
+# part 1
+print(min(mhdist(c) for c in crossings))
+
+# part 2
+print(min(w1.index(c) + 1 + w2.index(c) + 1 for c in crossings))
