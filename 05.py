08cb87358e77af041c3a958886c4b338c43b2760
Johan Herland <johan@herland.net>
2019-12-05 16:29:57 +0100

    Cleanup, day 5

--- 05.py
+++ 05.py
@@ -1,82 +1,80 @@
 from functools import partial
-from itertools import product
+from operator import add, eq, lt, mul, ne
 
 
-def args(nargs, nresults, program, ip, param_modes):
-    args = program[ip + 1:ip + 1 + nargs + nresults]
+def decode(opcode):
+    modebits, op = divmod(opcode, 100)  # mode bits: 0: position, 1: immediate
+    return op, [int(digit) for digit in '{:02d}'.format(modebits)[::-1]]
+
+
+def args(nargs, nresults, memory, ip, param_modes):
+    args = memory[ip + 1:ip + 1 + nargs + nresults]
+    arg_decoders = [lambda arg: memory[arg], lambda arg: arg]
     # Results are always addresses that we need to return (as if 'immediate')
-    modes = param_modes[:nargs] + ['imm'] * nresults
-    resolve_arg = {'pos': lambda arg: program[arg], 'imm': lambda arg: arg}
-    return [resolve_arg[mode](arg) for arg, mode in zip(args, modes)]
+    modes = param_modes[:nargs] + [1] * nresults
+    return [arg_decoders[mode](arg) for arg, mode in zip(args, modes)]
 
 
-def binary_op(do_op, program, ip, param_modes, input):
-    a, b, result = args(2, 1, program, ip, param_modes)
-    #  print('Combining', a, 'and', b, '->', result)
-    program[result] = do_op(a, b)
+def binary_op(do_op, memory, ip, param_modes):
+    a, b, result = args(2, 1, memory, ip, param_modes)
+    memory[result] = do_op(a, b)
     return ip + 4
 
 
-def do_input(program, ip, param_modes, input):
-    addr, = args(0, 1, program, ip, param_modes)
-    program[addr] = input
+def do_input(memory, ip, param_modes):
+    addr, = args(0, 1, memory, ip, param_modes)
+    memory[addr] = memory.pop()  # input is passed through at end of memory
     return ip + 2
 
 
-def do_output(program, ip, param_modes, input):
-    arg = args(1, 0, program, ip, param_modes)
-    print('Output:', arg)
+def do_output(memory, ip, param_modes):
+    arg, = args(1, 0, memory, ip, param_modes)
+    print(arg)
     return ip + 2
 
 
-def conditional_jump(predicate, program, ip, param_modes, input):
-    condition, target = args(2, 0, program, ip, param_modes)
+def conditional_jump(predicate, memory, ip, param_modes):
+    condition, target = args(2, 0, memory, ip, param_modes)
     return target if predicate(condition) else ip + 3
 
 
-def binary_test(predicate, program, ip, param_modes, input):
-    a, b, result = args(2, 1, program, ip, param_modes)
-    program[result] = 1 if predicate(a, b) else 0
+def binary_test(predicate, memory, ip, param_modes):
+    a, b, result = args(2, 1, memory, ip, param_modes)
+    memory[result] = 1 if predicate(a, b) else 0
     return ip + 4
 
 
-def end(program, ip, param_modes, input):
+def end(memory, ip, param_modes):
     raise StopIteration
 
 
 ops = {
-    1: partial(binary_op, lambda a, b: a + b),
-    2: partial(binary_op, lambda a, b: a * b),
+    1: partial(binary_op, add),
+    2: partial(binary_op, mul),
     3: do_input,
     4: do_output,
-    5: partial(conditional_jump, lambda arg: arg != 0),
-    6: partial(conditional_jump, lambda arg: arg == 0),
-    7: partial(binary_test, lambda a, b: a < b),
-    8: partial(binary_test, lambda a, b: a == b),
+    5: partial(conditional_jump, partial(ne, 0)),
+    6: partial(conditional_jump, partial(eq, 0)),
+    7: partial(binary_test, lt),
+    8: partial(binary_test, eq),
     99: end,
 }
 
 
-def decode(opcode):
-    modebits, op = divmod(opcode, 100)
-    modedecode = {'0': 'pos', '1': 'imm'}
-    return op, [modedecode[c] for c in '{:03d}'.format(modebits)[::-1]]
-
-
 def run(program, noun=None, verb=None, ip=0, input=None):
-    program = program[:]
+    memory = program[:]
+    if input is not None:  # Pass through input at end of memory
+        memory.append(input)
     if noun is not None:
-        program[1] = noun
+        memory[1] = noun
     if verb is not None:
-        program[2] = verb
+        memory[2] = verb
     try:
         while True:
-            #  print('  Next:', program[ip:ip + 4])
-            op, param_modes = decode(program[ip])
-            #  print('Op:', op, 'Modes:', param_modes)
-            ip = ops[op](program, ip, param_modes, input)
+            op, param_modes = decode(memory[ip])
+            ip = ops[op](memory, ip, param_modes)
     except StopIteration:
-        return program[0]
+        return memory[0]
 
 
 with open('05.input') as f:
