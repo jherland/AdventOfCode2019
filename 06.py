4b429ea9ce6c91daf251a0f76a58f870a62a8dbf
Johan Herland <johan@herland.net>
2019-12-06 12:22:08 +0100

    Cleanup. Slower but shorter

--- 06.py
+++ 06.py
@@ -1,42 +1,20 @@
-def parse_orbits(f):
-    for line in f:
-        parent, child = line.rstrip().split(')')
-        assert (len(parent), len(child)) == (3, 3)
-        yield parent, child
-
-
-orbits = {}  # map parent -> set of children
 parents = {}  # child -> parent
 
 with open('06.input') as f:
-    for p, c in parse_orbits(f):
-        orbits.setdefault(p, set()).add(c)
-        parents[c] = p
-
-depths = {}
+    for line in f:
+        parent, child = line.rstrip().split(')')
+        parents[child] = parent
 
 
-def find_depths(parent, depth):
-    depths[parent] = depth
-    for child in orbits.get(parent, set()):
-        find_depths(child, depth + 1)
-
-
-find_depths('COM', 0)
-
-
-def up(obj):
+def to_root(obj):
     if obj in parents:
         parent = parents[obj]
         yield parent
-        yield from up(parent)
+        yield from to_root(parent)
 
 
 # part 1
-print(sum(depths.values()))
+print(sum(len(list(to_root(obj))) for obj in parents.keys()))
 
 # part 2
-you_to_root = list(up('YOU'))
-san_to_root = list(up('SAN'))
-
-print(len(set(you_to_root).symmetric_difference(set(san_to_root))))
+print(len(set(to_root('YOU')).symmetric_difference(set(to_root('SAN')))))
