af52d7771aab7e3e03af7d4e1cd0db821ebe1595
Johan Herland <johan@herland.net>
2019-12-15 01:32:12 +0100

    Improved Day 14 in Python. Better in ALL THE WAYS

--- 14.py
+++ 14.py
@@ -1,136 +1,61 @@
-from dataclasses import dataclass
+from collections import defaultdict, namedtuple
 from math import ceil
-from typing import Set
 
-
-@dataclass(order=True, frozen=True)
-class Chemical:
-    unit: str
-    quantity: int
-
-    def __str__(self):
-        return f'{self.quantity} {self.unit}'
-
-    def __hash__(self):
-        return hash(self.unit)  # DON'T INCLUDE QUANTITY
-
-    def __add__(self, other):
-        assert other.unit == self.unit
-        return Chemical(self.unit, self.quantity + other.quantity)
-
-    def __sub__(self, other):
-        assert other.unit == self.unit
-        assert other.quantity <= self.quantity
-        return Chemical(self.unit, self.quantity - other.quantity)
-
-    def __mul__(self, num):
-        assert isinstance(num, int)
-        return Chemical(self.unit, self.quantity * num)
-
-    @classmethod
-    def parse(cls, s):
-        quantity, unit = s.split(' ')
-        return cls(unit, int(quantity))
-
-
-@dataclass
-class Reaction:
-    makes: Chemical
-    needs: Set[Chemical]
-
-    def __str__(self):
-        return f'{self.makes} <= ' + ', '.join(map(str, self.needs))
-
-    @classmethod
-    def parse(cls, line):
-        needs, makes = line.rstrip().split(' => ')
-        needs = needs.split(', ')
-        return cls(Chemical.parse(makes), {Chemical.parse(n) for n in needs})
-
-    def takes(self, unit):
-        return any(unit == c.unit for c in self.needs)
-
-    def instances(self, chemical):
-        """How many instances of this are needed to make the given quantity"""
-        assert chemical.unit == self.makes.unit
-        return ceil(chemical.quantity / self.makes.quantity)
-
-    def ingredients(self, instances):
-        """Return the ingredients needed to make the given chemical"""
-        return {ingr * instances for ingr in self.needs}
-
-
-class Factory:
-    def __init__(self, reactions, *initial_ingredients):
-        self.reactions = reactions
-        self.vat = {}  # map unit -> Chemical
-        for chemical in initial_ingredients:
-            self.add(chemical)
-
-    def __str__(self):
-        return ', '.join(map(str, self.vat.values()))
-
-    def __getitem__(self, unit):
-        return self.vat.get(unit, Chemical(unit, 0))
-
-    def add(self, chemical):
-        self.vat[chemical.unit] = self[chemical.unit] + chemical
-
-    def take(self, chemical):
-        #  print(f'  take({chemical}) from {self}')
-        while self[chemical.unit] < chemical:  # must produce
-            need = chemical - self[chemical.unit]
-            #  half = Chemical(need.unit, ceil(need.quantity * 9 / 10))
-            self.produce(need)
-        self.vat[chemical.unit] -= chemical
-        if self.vat[chemical.unit].quantity == 0:
-            del self.vat[chemical.unit]
-        return chemical
-
-    def produce(self, chemical):
-        #  print(f'  produce({chemical}) from {self}')
-        reaction = self.reactions[chemical.unit]
-        instances = reaction.instances(chemical)
-        taken = set()
-        try:
-            for ingredient in reaction.ingredients(instances):
-                taken.add(self.take(ingredient))
-        except KeyError:  # Not enough ingredients
-            for ingredient in taken:
-                self.add(ingredient)
-            raise
-        self.add(reaction.makes * instances)
+Chemical = namedtuple('Chemical', ['amount', 'unit'])
+Reaction = namedtuple('Reaction', ['ins', 'out'])
 
 
 def parse(f):
+    def parsechem(s):
+        amount, unit = s.strip().split(' ')
+        return Chemical(int(amount), unit)
+
     for line in f:
-        r = Reaction.parse(line)
-        yield r.makes.unit, r
+        ins, out = line.split('=>')
+        yield Reaction({parsechem(i) for i in ins.split(',')}, parsechem(out))
 
 
 with open('14.input') as f:
-    reactions = dict(parse(f))
+    reactions = {r.out.unit: r for r in parse(f)}  # map unit -> Reaction
+
+distances = {'ORE': 0}  # sort units by max reaction distance from ORE
+while len(distances) <= len(reactions):
+    for unit in set(reactions.keys()) - set(distances.keys()):
+        input_units = (i.unit for i in reactions[unit].ins)
+        try:
+            distances[unit] = max(distances[iu] for iu in input_units) + 1
+        except KeyError:
+            pass
+
+
+def ore_needed(fuel):
+    needed = defaultdict(int)
+    needed['FUEL'] = fuel
+    while needed:
+        unit = max(needed.keys(), key=lambda k: distances[k])
+        amount = needed[unit]
+        if unit == 'ORE':
+            return amount
+        del needed[unit]
+        instances = ceil(amount / reactions[unit].out.amount)
+        for i in reactions[unit].ins:
+            needed[i.unit] += i.amount * instances
+
+
+def bsearch(lo, hi, too_high):
+    mid = lo
+    while hi > lo + 1:
+        mid = lo + (hi - lo) // 2
+        if too_high(mid):
+            hi = mid
+        else:
+            lo = mid
+    return mid
+
 
 # part 1
-init_ore = Chemical('ORE', 1000000000000)
-f = Factory(reactions, init_ore)
-fuel = f.take(Chemical('FUEL', 1))
-print(init_ore - f['ORE'])
+print(ore_needed(1))
 
 # part 2
-try:
-    while fuel < Chemical('FUEL', 2000000):
-        fuel += f.take(Chemical('FUEL', 10000))
-except KeyError:
-    pass
-try:
-    while fuel < Chemical('FUEL', 2800000):
-        fuel += f.take(Chemical('FUEL', 100))
-except KeyError:
-    pass
-try:
-    while True:
-        fuel += f.take(Chemical('FUEL', 1))
-except KeyError:
-    pass
-print(fuel)
+ore = 1000000000000
+print(bsearch(1, ore, lambda fuel: ore_needed(fuel) > ore))
