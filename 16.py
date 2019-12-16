38203b81b58c9bbe499118d6307ece1978246d38
Johan Herland <johan@herland.net>
2019-12-16 23:15:33 +0000

    Day 16: Use arrays of small ints to reduce memory footprint
    Now completes in <1min and <5% RAM.

--- 16.py
+++ 16.py
@@ -1,19 +1,18 @@
+from array import array
 from itertools import chain, cycle, islice, repeat
 
 
 def fft(digits, offset=0, length=8, repeats=1):
-    digits = [int(d) for d in digits]
-    total_len = len(digits) * repeats
-    cache = {}  # map (depth, i) -> digit
-    for i, d in enumerate(digits * repeats):  # fill top row
-        cache[(0, i)] = d
-    for depth in range(100):  # fill rightmost column
-        cache[(depth + 1, total_len - 1)] = cache[(depth, total_len - 1)]
-    filled = [0] + [total_len - 1] * 100
+    width = len(digits) * repeats
+    rows = [array('b', islice(cycle([int(d) for d in digits]), width))]
+    rows += [array('b', repeat(-1, width)) for _ in range(100)]
+    for y in range(100):  # fill in rightmost column
+        rows[y + 1][-1] = rows[y][-1]
+    rowfill = [0] + [width - 1] * 100
 
     def fft_pattern(reps):
         pattern = chain.from_iterable(repeat(d, reps) for d in [0, 1, 0, -1])
-        return islice(cycle(pattern), 1, total_len + 1)
+        return islice(cycle(pattern), 1, width + 1)
 
     def fft_digit_slow(depth, i):
         total = sum(
@@ -22,25 +21,26 @@ def fft(digits, offset=0, length=8, repeats=1):
         return abs(total) % 10
 
     def fft_digit_fast(depth, i):
-        assert i >= total_len / 2
-        if filled[depth - 1] > i:
+        assert i >= width / 2
+        if rowfill[depth - 1] > i:
             fft_digit_fast(depth - 1, i)  # compute row above
-        for j in reversed(range(i, filled[depth])):
-            total = cache[(depth, j + 1)] + cache[(depth - 1, j)]
-            cache[(depth, j)] = abs(total) % 10
-        filled[depth] = i
-        return cache[(depth, i)]
+        for j in reversed(range(i, rowfill[depth])):
+            total = rows[depth][j + 1] + rows[depth - 1][j]
+            rows[depth][j] = abs(total) % 10
+        rowfill[depth] = i
+        return rows[depth][i]
 
     def fft_digit(depth, i):
-        try:
-            return cache[(depth, i)]
-        except KeyError:
-            assert depth > 0 and i < filled[depth]
-        if i >= total_len / 2:  # In the right half
+        result = rows[depth][i]
+        if result >= 0:
+            return result
+        else:
+            assert depth > 0 and i < rowfill[depth]
+        if i >= width / 2:  # In the right half
             return fft_digit_fast(depth, i)
         else:
-            cache[(depth, i)] = fft_digit_slow(depth, i)
-            return cache[(depth, i)]
+            rows[depth][i] = fft_digit_slow(depth, i)
+            return rows[depth][i]
 
     return [fft_digit(100, i) for i in range(offset, offset + length)]
 
