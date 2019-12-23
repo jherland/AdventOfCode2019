d5ccef62b2d4ac98e38e9059a68a82fa4c22c46b
Johan Herland <johan@herland.net>
2019-12-23 00:20:59 +0000

    Part 2 in Python

--- 22.py
+++ 22.py
@@ -1,29 +1,10 @@
-from functools import partial
-
-
-def deal_w_i(inc, deck):
-    new = [None] * len(deck)
-    for i, card in enumerate(deck):
-        new[i * inc % len(deck)] = card
-    assert None not in new
-    return new
-
-
-def deal(deck):
-    return list(reversed(deck))
-
-
-def cut(num, deck):
-    return deck[num:] + deck[:num]
-
-
 def parse_instruction(line):
     if line.startswith('deal with increment '):
-        return partial(deal_w_i, int(line[20:]))
+        return 'deal_w_i', int(line[20:])
     elif line == 'deal into new stack':
-        return deal
+        return 'deal',
     elif line.startswith('cut '):
-        return partial(cut, int(line[4:]))
+        return 'cut', int(line[4:])
     else:
         raise NotImplementedError(line)
 
@@ -34,12 +15,45 @@ def parse(f):
             yield parse_instruction(line.strip())
 
 
+def inv(mod, n):
+    return pow(n, mod - 2, mod)
+
+
+def update(mod, off, inc, instr, *arg):
+    ops = {
+        'deal': lambda: (off - inc, -inc),
+        'cut': lambda n: (off + inc * n, inc),
+        'deal_w_i': lambda n: (off, inc * inv(mod, n)),
+    }
+    return tuple(n % mod for n in ops[instr](*arg))
+
+
+def find_linear_func(mod, instructions):
+    off, inc = 0, 1
+    for instr in instructions:
+        off, inc = update(mod, off, inc, *instr)
+    return off, inc
+
+
+def apply_linear_func_n_times(off, inc, mod, n):
+    total_inc = pow(inc, n, mod)
+    total_off = off * (1 - total_inc) * inv(mod, 1 - inc)
+    return total_off, total_inc
+
+
 with open('22.input') as f:
     instructions = list(parse(f))
 
-deck = list(range(10007))
-
 # part 1
-for instr in instructions:
-    deck = instr(deck)
-print(deck.index(2019))
+deck_size = 10007
+card = 2019
+off, inc = find_linear_func(deck_size, instructions)
+print(((card - off) * inv(deck_size, inc)) % deck_size)
+
+# part 2
+deck_size = 119315717514047
+instr_times = 101741582076661
+pos = 2020
+off, inc = find_linear_func(deck_size, instructions)
+off, inc = apply_linear_func_n_times(off, inc, deck_size, instr_times)
+print((off + pos * inc) % deck_size)
